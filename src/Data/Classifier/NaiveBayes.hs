1803688d370f13dfe373b49669dd89e1ba53c74f
Fraser Murray <fraser.m.murray@gmail.com>
2015-03-10 00:04:26 +0000

    fix naive-bayes

--- src/Data/Classifier/NaiveBayes.hs
+++ src/Data/Classifier/NaiveBayes.hs
@@ -2,77 +2,44 @@ module Data.Classifier.NaiveBayes
   ( test
   , probabilities
   , priors
-  , conditionalProbability ) where
+  , conditionalProbabilities
+  , module Data.Classifier ) where
 
 import Data.Classifier
 import Data.Counter (Counter(Counter))
+import Data.List
 import Data.Map (Map)
 import Data.Monoid
+import Data.Ord
 import Data.Ratio ((%))
 import qualified Data.Counter as Counter
 import qualified Data.Map.Strict as Map
 
-test :: (Ord a, Ord b) => Classifier a b -> Counter b -> Maybe a
+test :: (Ord a, Ord b, Show b) => Classifier a b -> Counter b -> Maybe a
 test cls cnt =
-  case Map.toList $ probabilities cls cnt of
+  case sortBy (comparing (Down . snd)) $ Map.toList $ probabilities cls cnt of
     [] -> Nothing
     (x, _):_ -> Just x
 
 probabilities :: (Ord a, Ord b) => Classifier a b -> Counter b -> Map a Rational
 probabilities cls =
-  applyMultinomial (priors cls) (conditionalProbability cls)
+  applyMultinomial (priors cls) (conditionalProbabilities cls)
 
-conditionalProbability :: (Ord a, Ord b) => Classifier a b -> Map a (Map b Rational)
-conditionalProbability (Classifier m) =
-  fM (tct $ Classifier m) $ \cls cnt ->
-    Map.insert cls
-      (Map.mergeWithKey
-        (\_ numInVocabForWord numInClassForWord -> Just $ numInClassForWord % numInVocabForWord)
-        (fmap (1%))
-        (const Map.empty)
-        ws' cnt)
-  where
-    fM x f = Map.foldrWithKey f Map.empty x
-    ws' = Counter.toMap $ mconcat $ mconcat $ Map.elems m
+conditionalProbabilities :: (Ord a, Ord b) => Classifier a b -> (Map b Integer, Map a Integer, Map a (Map b Rational))
+conditionalProbabilities (Classifier m) =
+  (combined, totalWordsInClass, Map.intersectionWith (\b t -> fmap (\x -> (x + 1) % b) t) divisor wordCountInClass)
+  where totalWordsInClass = fmap (Counter.total . mconcat) m
+        wordCountInClass = fmap (Counter.toMap . mconcat) m
+        combined = Counter.toMap $ Map.foldr (<>) mempty $ fmap mconcat m
+        totalCombined = Map.foldr (const (+1)) 0 combined
+        divisor = fmap (+ totalCombined) totalWordsInClass
 
 priors :: (Ord a, Ord b) => Classifier a b -> Map a Rational
 priors c@(Classifier m) = fmap (\cs -> fromIntegral (length cs) % documentCount c) m
 
-applyMultinomial :: (Ord a, Ord b) => Map a Rational -> Map a (Map b Rational) -> Counter b -> Map a Rational
-applyMultinomial prior condprob (Counter.toMap -> m) = f
+applyMultinomial :: (Ord a, Ord b) => Map a Rational -> (Map b Integer, Map a Integer, Map a (Map b Rational)) -> Counter b -> Map a Rational
+applyMultinomial prior (global, classCount, condprobs) (Counter.toMap -> m) = f
   where
-    f = Map.intersectionWith (*) prior $ fmap product h
-    h = fmap (\n -> Map.elems $ Map.intersectionWith (\x y -> x * fromIntegral y) n m) condprob
-
-tct :: (Ord a, Ord b) => Classifier a b -> Map a (Map b Integer)
-tct (Classifier m) = fmap (Counter.toMap . mconcat) m
-
--- def train(classes, data):
---   words = extract_vocab(data)
---   n = count_docs(data)
---   prior = {}
---   condprob = {}
---   for class in classes:
---     condprob[class] = {}
---     nc = count_docs_in_class(data, class)
---     prior[class] = nc / n
---     class_text = docs_in_class(data, class).concat_words()
---     for t in words:
---       token_count = count_tokens_of_term(class_text, t)
---       condprob[class][t] = (token_count + 1) / (sum [class_text[t2][c] for t2 in words])
---    return V, prior, condprob
-
--- TRAINMULTINOMIALNB(C, D)
--- 1 V ← EXTRACTVOCABULARY(D)
--- 2 N ← COUNTDOCS(D)
--- 3 for each c ∈ C
--- 4 do Nc ← COUNTDOCSINCLASS(D, c)
--- 5 prior[c] ← Nc/N
--- 6 textc ← CONCATENATETEXTOFALLDOCSINCLASS(D, c)
--- 7 for each t ∈ V
--- 8 do Tct ← COUNTTOKENSOFTERM(textc, t)
--- 9 for each t ∈ V
--- 10 do condprob[t][c] ← Tct+1
--- ∑t
--- ′(Tct′+1)
--- 11 return V, prior, condprob
+    f = Map.intersectionWith (*) prior $ fmap (Map.foldr (*) 1) qwer
+    qwer = Map.mergeWithKey (\_ condprob r -> Just $ Map.mergeWithKey (\_ p t -> Just $ p ^ t ) (const mempty) (fmap (\w -> (1 % (r + total)) ^ w)) condprob m) (const mempty) (const mempty) condprobs classCount
+    total = Map.foldr (const (+1)) (0 :: Integer) global
